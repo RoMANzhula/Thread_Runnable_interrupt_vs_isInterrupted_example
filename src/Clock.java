2889a3e27d8be00a3e06b39d1f1ed99b67766c2d
Roman Manzhula <roman1985al@ukr.net>
2024-12-25 18:46:43 +0200

    Update Clock.java
    In Ukrainian

--- src/Clock.java
+++ src/Clock.java
@@ -3,26 +3,24 @@ import java.sql.Time;
 import java.time.LocalDateTime;
 
 public class Clock implements Runnable {
-    @Override //обязательный метод для реализации класса Runnable
+    @Override //обов'язковий метод для реалізації інтерфейсу Runnable
     public void run() {
-        Thread current = Thread.currentThread(); //для текущего объекта данного класса получаем текущий объект вызвавшей его нити
+        Thread current = Thread.currentThread(); //для поточного об'єкта цього класу отримуємо поточний об'єкт потоку, що викликав його
 
-        while (!current.isInterrupted()) { //пока данный поток не был прерван (т.е. пока метод проверки isInterrupted() = false)
-            try { //будем пытаться
-                Thread.sleep(1000); //усыплять поток на 1 секунду
-            } catch (InterruptedException interruptedException) { //пытаемся ловить прерванное исключение
-                interruptedException.printStackTrace(); //выводим стек для данного исключения
-                current.interrupt(); //для данного потока вызываем метод interrupt(), чтобы установить потоку статус -
-                // прерван (чтоб вернуть true)
+        while (!current.isInterrupted()) { //поки цей потік не буде перерваний (тобто, поки метод перевірки isInterrupted() повертає false)
+            try { //спробуємо
+                Thread.sleep(1000); //засинаємо потік на 1 секунду
+            } catch (InterruptedException interruptedException) { //спробуємо ловити перерване виключення
+                interruptedException.printStackTrace(); //виводимо стек для цього виключення
+                current.interrupt(); //для цього потоку викликаємо метод interrupt(), щоб встановити потоку статус -
+                // перервано (щоб повернути true)
             }
 
-            if ((System.currentTimeMillis() / 1000) % 2 == 0) { //если текущее время в секундах при остатке от деления на 2 будет четным, то
-                System.out.println("Tik"); //выводим в консоль Tik
-            } else { //иначе
-                System.out.println("Tak"); //выводим в консоль Tak
+            if ((System.currentTimeMillis() / 1000) % 2 == 0) { //якщо поточний час у секундах при залишку від ділення на 2 буде парним, то
+                System.out.println("Tik"); //виводимо в консоль "Tik"
+            } else { //інакше
+                System.out.println("Tak"); //виводимо в консоль "Tak"
             }
-
-
         }
     }
 }
